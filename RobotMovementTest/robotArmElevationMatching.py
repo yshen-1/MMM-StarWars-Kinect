7242d8f1888e4758a467c1475bc191f699166e76
roboticsenthusiast <alanyuyi98@gmail.com>
2016-11-19 20:12:52 -0500

    Finished robot arm elevation code

--- RobotMovementTest/robotArmElevationMatching.py
+++ RobotMovementTest/robotArmElevationMatching.py
@@ -1,7 +1,7 @@
 from pykinect2 import PyKinectV2, PyKinectRuntime
 from pykinect2.PyKinectV2 import *
-import math
-
+import math, threading, time, sys
+from MMM import MMM
 class kinectHandler(object):
 	def __init__(self):
 		self.kinect=PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Body)
@@ -19,5 +19,28 @@ class kinectHandler(object):
 					rightHandAngle=math.atan(rightHandY/rightHandZ)
 					return rightHandAngle
 
-
-
+class robotTracker(object):
+	def __init__(self):
+		self.handler=kinectHandler()
+		self.mmm=MMM('COM3')
+		time.sleep(4)
+		self.thread = threading.Thread(target=self.updateRobot, args=())
+		self.thread.daemon = True            
+		self.thread.start()
+	def updateRobot(self):
+		while True:
+			self.mmm.clampAll();  
+			self.mmm.update();
+			time.sleep(0.1);
+			self.mmm.parseData();
+	def run(self):
+		while True:
+			angle=self.handler.getRightArmElevation()
+			if angle!=None:
+				angle=angle*180/math.pi
+				if angle>60: angle=60
+				elif angle<-60: angle=-60
+				self.mmm.rotateElbows(0,angle)
+				time.sleep(2)
+		self.mmm.ser.close()
+		quit()   
