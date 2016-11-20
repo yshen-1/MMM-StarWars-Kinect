ee31a2a7895ff0f7150a19e55c7ce57e26c63295
roboticsenthusiast <alanyuyi98@gmail.com>
2016-11-19 19:38:04 -0500

    Fixed code for getting right hand angle elevation

--- KinectTest1MMM/KinectRightArmAngleTest.py
+++ KinectTest1MMM/KinectRightArmAngleTest.py
@@ -3,19 +3,18 @@ from pykinect2.PyKinectV2 import *
 import math
 kinect=PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Body)
 def getRightArmElevation():
-	
 	bodies=None
 	if kinect.has_new_body_frame():
 		bodies=kinect.get_last_body_frame()
 	if bodies!=None:
-		body=bodies[-1]
-		joints=body.joints
-		rightHandY=joints[PyKinectV2.JointType_HandRight].Position.y
-		rightHandZ=joints[PyKinectV2.JointType_HandRight].Position.z
-		rightHandAngle=math.atan(rightHandY/rightHandZ)
-		return rightHandAngle
+		for i in range(0,kinect.max_body_count):
+			body=bodies.bodies[i]
+			if body.is_tracked:
+				joints=body.joints
+				rightHandY=joints[PyKinectV2.JointType_HandRight].Position.y
+				rightHandZ=joints[PyKinectV2.JointType_HandRight].Position.z
+				rightHandAngle=math.atan(rightHandY/rightHandZ)
+				return rightHandAngle
 
 
-while True:
-	angle=getRightArmElevation()
-	print(angle)
+
