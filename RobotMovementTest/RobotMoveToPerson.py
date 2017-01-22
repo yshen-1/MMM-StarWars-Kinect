ac04be0ac7195e1dc7065f7c2a351d968c4529ac
Arthur Levy <afl1@andrew.cmu.edu>
2017-01-21 19:54:35 -0500

    Added Kinect connection check

--- RobotMovementTest/RobotMoveToPerson.py
+++ RobotMovementTest/RobotMoveToPerson.py
@@ -1,9 +1,23 @@
+# To download PyUSB with conda:
+# conda install -c trentonoliphant pyusb=1.0.0b2
+# conda install -c m-labs libusb=1.0.20
 import math
 import numpy as np
+import usb.core
+import usb.util
 from pykinect2 import PyKinectV2, PyKinectRuntime
 from pykinect2.PyKinectV2 import *
+
+
 def checkIfKinectConnected():
-    return True
+    # Actually checks if a microsoft device is connected
+    # Microsoft vendorID = 0x045e
+    device = usb.core.find(idVendor = 0x045e)
+    if device != None:
+        return True
+    else:
+        return False
+
 class KinectHandler(object):
     def __init__(self):
         self.kinectBodyStream=PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Body)
@@ -24,16 +38,19 @@ class KinectHandler(object):
                 distanceToUser = (hipX**2+hipZ**2)**0.5
                 theta =  math.atan(hipX/hipZ)
                 return (distanceToUser, theta)
+
     def getNewDepthData(self):
         #Frame gets returned as numpy array
         frame=None
         if self.kinectDepthStream.has_new_depth_frame():
             frame=self.kinectDepthStream.get_last_depth_frame()
         return frame
+
     def end(self):
         self.kinectBodyStream.close()
         self.kinectDepthStream.close()
         self.kinectColorStream.close()
+
 class humanTracker(object):
     def __init__(self):
         self.kinect=KinectHandler()
