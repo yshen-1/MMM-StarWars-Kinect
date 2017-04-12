cc7cf1ea09cff960b63ef330a3f145ee2b58983c
roboticsenthusiast <alanyuyi98@gmail.com>
2017-04-12 00:07:36 -0400

    Finished person tracker for demo

--- RobotMovementTest/demo.py
+++ RobotMovementTest/demo.py
@@ -3,24 +3,25 @@
 # conda install -c trentonoliphant pyusb=1.0.0b2
 # conda install -c m-labs libusb=1.0.20
 from __future__ import print_function,division
-import math, copy
+import math, copy, Queue, threading, random, cv2, time, sys
 import numpy as np
 import usb.core
 import usb.util
-import pygame
-import Queue
-import threading
-import random
-import cv2
-import time
 from pykinect2 import PyKinectV2, PyKinectRuntime
 from pykinect2.PyKinectV2 import *
+import matplotlib.pyplot as plt
+from MMM import MMM
 from findLightSaber import LightSaberTracker
 '''
 IMPORTANT: Kinect depth frame contents are full depth values in mm.
 They range up to 4000 mm. NO NEED FOR BITWISE SHIFT.
 
 TO GET DIMENSIONS OF DEPTH FRAME, USE self.kinectDepthStream.depth_frame_desc.Width, ''.Height
+
+The coordinate system used in the fighting function is the same as the skeleton
+space coordinate system. X,Y,Z from the depth stream are in mm.
+
+ALL DATA ACQUIRED FROM BODY DATA STREAM IS IN METERS.
 '''
 def rgbString(red, green, blue):
     #http://www.cs.cmu.edu/~112/notes/notes-graphics.html#customColors
@@ -42,12 +43,13 @@ def checkIfKinectConnected():
 #Currently displays depth image from kinect that is repeated
 ##############################################################################
 
-def convertTo2DGrid(oneDArray,cols=640):
+def convertTo2DGrid(oneDArray,cols):
     grid=[]
     while(len(oneDArray)>0):
-        grid.append(oneDArray[:cols])
+        grid.append(list(reversed(oneDArray[:cols])))
         del oneDArray[:cols]
     return np.array(grid)
+
 def makeArrayRectangular(targetArray):
     #Makes sure last row of 2D array matches row 1
     targetCols=len(targetArray[0])
@@ -55,10 +57,21 @@ def makeArrayRectangular(targetArray):
     newArray=targetArray.tolist()
     newArray[-1].extend([0]*missingCols)
     return newArray
+
+
 def convertColorFrameIntoRGBArray(frame,width,height):
     RGBframe=np.delete(frame,slice(3,None,4))
     RGBframe=np.reshape(RGBframe,(height,width,3))
     return RGBframe
+
+def distance(x1,x2,y1,y2):
+    return (((x1-x2)**2)+((y1-y2)**2))**0.5
+class humanObject(object):
+    def __init__(self,hipPosition,rightWrist,rightElbow,rightShoulder):
+        self.hip=hipPosition
+        self.wrist=rightWrist
+        self.elbow=rightElbow
+        self.shoulder=rightShoulder
 
 class KinectHandler(object):
     def __init__(self):
@@ -68,8 +81,24 @@ class KinectHandler(object):
                                PyKinectV2.FrameSourceTypes_Depth)
         self.kinectColorStream=PyKinectRuntime.PyKinectRuntime(
                                PyKinectV2.FrameSourceTypes_Color)
-
-    def getPeoplePosition(self):
+    def getBodyPositions(self):
+        bodies=None
+        if self.kinectBodyStream.has_new_body_frame():
+            bodies=self.kinectBodyStream.get_last_body_frame()
+        if bodies!=None:
+            bodyList=[]
+            for body in bodies.bodies:
+                if body.is_tracked:
+                    joints = body.joints
+                    hip = joints[PyKinectV2.JointType_SpineBase]
+                    rightWrist=joints[PyKinectV2.JointType_WristRight]
+                    rightElbow=joints[PyKinectV2.JointType_ElbowRight]
+                    rightShoulder=joints[PyKinectV2.JointType_ShoulderRight]
+                    newBody=humanObject(hip.Position,rightWrist.Position,
+                                        rightElbow.Position,rightShoulder.Position)
+                    bodyList.append(newBody)
+            return bodyList
+    def getPersonPosition(self): #Use after initial approach
         bodies=None
         if self.kinectBodyStream.has_new_body_frame():
             bodies=self.kinectBodyStream.get_last_body_frame()
@@ -80,23 +109,9 @@ class KinectHandler(object):
                     hip = joints[PyKinectV2.JointType_SpineBase]
                     hipX = hip.Position.x
                     hipZ = hip.Position.z
-                    distanceToUser = (hipX**2+hipZ**2)**0.5
-                    theta =  math.atan(hipX/hipZ)
-                    playerIndex = bodies.bodies.index(body)
-                    return (distanceToUser, theta, playerIndex)
-    def getRightWristPosition(self):
-        bodies=None
-        if self.kinectBodyStream.has_new_body_frame():
-            bodies=self.kinectBodyStream.get_last_body_frame()
-        if bodies!=None:
-            for i in range(0,self.kinectBodyStream.max_body_count):
-                body=bodies.bodies[i]
-                if body.is_tracked:
-                    joints=body.joints
-                    rightWristX=joints[PyKinectV2.JointType_WristRight].Position.x
-                    rightWristY=joints[PyKinectV2.JointType_WristRight].Position.y
-                    rightWristZ=joints[PyKinectV2.JointType_WristRight].Position.z
-                    return rightWristX,rightWristY,rightWristZ #in meters
+                    theta =  (hipX/np.abs(hipX))*math.atan(np.abs(hipX)/hipZ)*180/math.pi
+                    return theta
+
     def getNewDepthData(self):
         #Returns 1D numpy array of 2 byte objects
         frame=None
@@ -111,40 +126,44 @@ class KinectHandler(object):
             frame=self.kinectColorStream.get_last_color_frame()
             return (frame, self.kinectColorStream.color_frame_desc.Width,
                     self.kinectColorStream.color_frame_desc.Height)
-
-    def convertColorToDepth(self,depthFrame,refToLargeArray):
-        self.kinectDepthStream.color_frame_to_camera_space(depthFrame,refToLargeArray)
-
     def end(self):
         self.kinectBodyStream.close()
         self.kinectDepthStream.close()
         self.kinectColorStream.close()
-
-class humanTracker():
+class Robot(object):
     def __init__(self):
         self.kinect=KinectHandler()
         self.isRunning=True
-        self.playerIndexBits=0
-        self.depthWidth,self.depthHeight=512,424
+        self.colorWidth,self.colorHeight=None,None
+        self.depthWidth,self.depthHeight=None,None
         self.depthFrame=None
-        self.originalDepthFrame=None
-        self.colorWidth,self.colorHeight=None, None
         self.colorFrame=None
         self.verticalDepthPixelRatio=424/60 #pixels per degree vertically
         self.horizontalDepthPixelRatio=512/70.6 #pixels per degree horizontally
         self.verticalColorPixelRatio=1080/53.8 #pixels per degree vertically
         self.horizontalColorPixelRatio=1920/84.1 #pixels per degree horizontally
         self.saberTracker=LightSaberTracker()
-    def stop(self):
-        self.isRunning=False
-    def getDepthLineAndFrame(self):
+        self.kinectX=0.1 #m between kinect and anatomical right side of robot
+        self.kinectY=1 #m from kinect to ground
+        self.mmm=MMM('COM4')
+        time.sleep(4)
+        self.mainThread=threading.Thread(target=self.updateRobot,args=())
+        self.mainThread.daemon=True
+        self.mainThread.start()
+    def updateRobot(self):
+        while True:
+            self.mmm.clampAll()
+            self.mmm.update()
+            time.sleep(0.1)
+            self.mmm.parseData()
+    def getDepthFrame(self):
         newDepthFrame=self.kinect.getNewDepthData()
         while newDepthFrame==None:
             newDepthFrame=self.kinect.getNewDepthData()
         (depthFrame,depthWidth,depthHeight)=newDepthFrame
         print("New depth frame acquired!")
-        self.depthWidth,self.depthHeight=depthWidth,depthHeight
-        self.originalDepthFrame=depthFrame
+        if self.depthWidth==None or self.depthHeight==None:
+            self.depthWidth,self.depthHeight=depthWidth,depthHeight
         depthContainer=list(copy.deepcopy(depthFrame))
         depthGrid=convertTo2DGrid(depthContainer,depthWidth)
         depthGrid=makeArrayRectangular(depthGrid)
@@ -154,7 +173,8 @@ class humanTracker():
         while newColorFrame==None:
             newColorFrame=self.kinect.getNewColorData()
         (colorFrame,colorWidth,colorHeight)=newColorFrame
-        self.colorWidth,self.colorHeight=colorWidth,colorHeight
+        if self.colorWidth==None or self.colorHeight==None:
+            self.colorWidth,self.colorHeight=colorWidth,colorHeight
         colorRGBFrame=convertColorFrameIntoRGBArray(colorFrame,colorWidth,colorHeight)
         self.colorFrame=copy.deepcopy(colorRGBFrame)#cv2.cvtColor(colorRGBFrame,cv2.COLOR_RGB2BGR)
     def skeletonSpaceToColor(self,x,y,z,cameraOffset): #x,y,z in meters
@@ -172,7 +192,7 @@ class humanTracker():
             rowY=((self.colorHeight/2)-(y/np.abs(y))*
                    self.verticalColorPixelRatio*(math.atan(np.abs(y)/z))*(180/math.pi))
         rowY=int(rowY)
-        return (rowY,colX)
+        return (rowY,colX) #Index in color frame
     def depthSpaceToSkeleton(self,row,col,z):
         z=z/1000
         if col==int(self.depthWidth/2):
@@ -188,68 +208,59 @@ class humanTracker():
                   *math.tan((np.abs((self.depthHeight/2)-row)/
                   self.verticalDepthPixelRatio)*math.pi/180))
         return (x,y,z) #in meters
-    def getTipAndHandData(self):
-        wristPos=self.kinect.getRightWristPosition()
-        while wristPos==None:
-            wristPos=self.kinect.getRightWristPosition()
-        (wristX,wristY,wristZ)=wristPos
-        print("Wrist relative to kinect center: ",wristPos)
-        self.getColorFrame()
-        colorFrameTaken=time.time()
-        kinectCameraOffset=9.5/100
-        (rowY,colX)=self.skeletonSpaceToColor(wristX,wristY,wristZ,kinectCameraOffset)
-        cv2.imwrite("ColorImageTaken.PNG",self.colorFrame)
-        coords=self.saberTracker.track(self.colorFrame,(rowY,colX))
-        if coords==None: return None
-        self.getDepthLineAndFrame()
-        depthFrameTaken=time.time()
-        print("Discrepancy: ",depthFrameTaken-colorFrameTaken)
-        (startingColorY,startingColorX)=coords[0]
-        transformedColorFrame=[[0]*1920 for i in range(1080)]
-        print("Saber start: ",startingColorY,startingColorX)
-        for j in range(len(self.depthFrame)):
-            for k in range(len(self.depthFrame[0])):
-                currentDepth=self.depthFrame[j][k]
-                if currentDepth==0: continue
-                (skelX,skelY,skelZ)=self.depthSpaceToSkeleton(j,k,currentDepth)
-                (colorRow,colorCol)=self.skeletonSpaceToColor(skelX,skelY,skelZ,kinectCameraOffset)
-                if (colorRow>=1080) or colorCol>=1920 or colorRow<0 or colorCol<0:
-                    #print(colorRow,colorCol," x: ",skelX," y: ",skelY," z: ",skelZ)
-                    continue
-                transformedColorFrame[colorRow][colorCol]=self.depthFrame[j][k]
-        transformedColorFrame=np.array(transformedColorFrame)
-        startTargetRegion=transformedColorFrame[startingColorY-10:startingColorY+11,startingColorX-10:startingColorX+10]
-        startAVGDistance=np.mean(startTargetRegion[startTargetRegion>0])
-        startTargetList=np.ravel(startTargetRegion)
-        startTargetList=np.array(filter(lambda x: x<startAVGDistance,startTargetList))
-        print("Saber start distance (mm): ",np.mean(startTargetList[startTargetList>0]))
-        print("Saber start list: ",startTargetList)
-        print(np.abs(wristZ-np.mean(startTargetList[startTargetList>0])/1000))
+    def getTargetPosition(self):
+        allHumans=self.kinect.getBodyPositions()
+        cameraOffset=9.5/100
+        for i in range(len(allHumans)):
+            self.getDepthFrame()
+            self.getColorFrame()
+            currentHuman=allHumans[i]
+            (rowY,colX)=self.skeletonSpaceToColor(currentHuman.wrist.x,
+                                                  currentHuman.wrist.y,
+                                                  currentHuman.wrist.z,
+                                                  cameraOffset)
+            lightSaberCoords=self.saberTracker.track(self.colorFrame,(rowY,colX))
+            if lightSaberCoords==None: continue
+            (hiltY,hiltX)=coords[0]
+            pixelDiff=distance(colX,hiltX,rowY,hiltY)
+            if pixelDiff<400:
+                return currentHuman
+        return False #target not found
 
-
-        b=raw_input("Press enter to continue.")
-        '''
-        tipXDiff=self.colorWidth/2-tipColorX
-        colorXAngle=np.abs(tipXDiff)/self.horizontalColorPixelRatio #Degrees
-
-        distanceToPerson=self.depthFrame[endDepthY][endDepthX]
-        return (distanceToPerson, depthX, depthY, endDepthX, endDepthY, endColorY,endColorX, wristX,wristY, wristZ, rowY,colX)
-        '''
-
+    def turnTowardsOnlyPerson(self):
+        angleToPerson=self.kinect.getPersonPosition()
+        while np.abs(angleToPerson)>20:
+            if angleToPerson>0: self.mmm.setWheelVelocity(-0.03,0.03)
+            else: self.mmm.setWheelVelocity(0.03,-0.03)
+            time.sleep(0.1)
+            self.mmm.setWheelVelocity(0,0)
+            angleToPerson=self.kinect.getPersonPosition()
+    def goToTarget(self):
+        target=self.getTargetPosition()
+        while target==None: target=self.getTargetPosition()
+        angleToTarget=math.atan(np.abs(target.hip.x)/target.hip.z)*180/math.pi
+        if target.hip.x>0: self.mmm.setWheelVelocity(-0.05,0.05)
+        else: self.mmm.setWheelVelocity(0.05,-0.05)
+        timeRequired=angleToTarget*(timePerDegree) #time required for robot to turn 1 degree
+        time.sleep(timeRequired)
+        distanceToTarget=(target.hip.x**2+target.hip.z**2)**0.5 # meters
+        self.mmm.setWheelVelocity(0.15,0.15)
+        timeForForward=(distanceToTarget-1)/0.15
+        time.sleep(timeForForward)
+        self.mmm.setWheelVelocity(0,0)
+        self.turnTowardsOnlyPerson()
     def run(self):
         while self.isRunning:
-            #DO STUFF
-            #(distanceToPerson, depthX, depthY, endDepthX, endDepthY, endColorY,endColorX, wristX,wristY, wristZ,wristRow,wristCol)=self.getTipAndHandData()
-            #colorContainer=list(copy.deepcopy(frame))
-            #depthGrid=convertTo2DGrid(depthContainer,width)
-            #print("Distance:",distanceToPerson)
-            self.getTipAndHandData()
+            self.goToTarget()
+            #self.fightPerson()
             if not checkIfKinectConnected():
                 print("Bye!")
                 self.isRunning=False
         self.kinect.end()
+        self.mmm.ser.close()
+        quit()
 
 
 if __name__=='__main__':
-    debugger=humanTracker()
-    debugger.run()
+    robot=Robot()
+    robot.run()
